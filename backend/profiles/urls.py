c25531d4940256d0d68a3de08f7cf93aa5b68d9a
Spectre <bowo@anakdesa.id>
2019-05-24 13:03:48 +0700

    Add Router

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+
 
 from . import views
 
-
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
+ 
 urlpatterns = [
-    path('hello/', views.HelloApiView, name='hello'),
+    path('hello/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
