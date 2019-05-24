f68012e7b6c1f6056de522bdb9a96a2529293df3
Spectre <bowo@anakdesa.id>
2019-05-24 13:03:39 +0700

    Add serializers

---
+++ backend/profiles/serializers.py
@@ -0,0 +1,35 @@
+
+from rest_framework import serializers
+
+from . import models
+
+
+
+class HelloSerializer(serializers.Serializer):
+    """Serializes a name field for testing our APIView"""
+
+    name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializes a user profile object"""
+
+    class Meta:
+        model = models.UserProfiles
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = models.UserProfiles.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+
+        return user
