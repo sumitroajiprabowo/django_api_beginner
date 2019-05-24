95d5f7d8f0369f329207d1255ec87e625dfe6053
Spectre <bowo@anakdesa.id>
2019-05-24 14:19:23 +0700

    Add Router login and feed

--- backend/profiles/urls.py
+++ backend/profiles/urls.py
@@ -8,7 +8,9 @@ from . import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
- 
+router.register('login', views.LoginViewset, base_name='login')
+router.register('feed', views.UserProfileFeedViewSet)
+
 urlpatterns = [
     path('hello/', views.HelloApiView.as_view()),
     path('', include(router.urls))
